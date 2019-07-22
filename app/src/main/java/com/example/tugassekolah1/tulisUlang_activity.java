a9a56cf4a2974bc639666c3c7fd3f5ed26ce3b21
redcountry15 <33802969+redcountry15@users.noreply.github.com>
2019-07-22 12:54:23 +0700

    Commit Baru

---
+++ app/src/main/java/com/example/tugassekolah1/tulisUlang_activity.java
@@ -0,0 +1,13 @@
+package com.example.tugassekolah1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tulisUlang_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tulis_ulang_activity);
+    }
+}
